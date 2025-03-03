18f99c300f0876a27491e47175f5b5d9f593795d
hieun <hieunhantran2002@gmail.com>
2025-03-03 01:46:23 -0500

    plot

--- src/Devoir2/cpp/graphique.py
+++ src/Devoir2/cpp/graphique.py
@@ -3,21 +3,47 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import os
 
+def C_ana(r, t) :
+    return 20*(np.exp(1e-10*t) - 1) * np.cos((np.pi / 2) * (r**2 / 0.5**2)) + 20
+
 def plot(filename) :
     data = pd.read_csv(filename)
     r = np.float64(np.array(data.columns)[1:])
-    plt.figure()
+    plt.figure(figsize=(10, 8))
+    colors = plt.cm.tab10(np.linspace(0, 1, len(data)))
     for i in np.linspace(0, len(data)-1, 10, dtype=int):
+        plt.plot(r[::111], np.array(data.iloc[i, 1:])[::111], "o", label=f't = {data.iloc[i, 0]:.2e}', color=colors[i])
+        plt.plot(r[::111], C_ana(r, data.iloc[i, 0])[::111], "--", color=colors[i])
+    
+    plt.plot([], [], "o", label=f'Numérique', color="black")
+    plt.plot([], [], "--", label=f'Analytique', color="black")
+    plt.xlabel(r'Rayon du pilier $r$ [m]')
+    plt.ylabel(r'Concentration $C$ [mol/m$^3$]')
+    plt.title(f'Profil de concentration de sel dans le pilier (N = {len(r[::111])})')
+    plt.legend()
+    plt.grid(zorder=0)
+    plt.tight_layout()
+    plt.show()
+
+def plot2(filename) :
+    data = pd.read_csv(filename)
+    r = np.float64(np.array(data.columns)[1:])
+    plt.figure()
+    plt.plot(r, np.array(data.iloc[0, 1:]), label=f't = {data.iloc[0, 0]:.2e}')
+    for i in np.logspace(0, np.log10(len(data)-1), 10, dtype=int):
         plt.plot(r, np.array(data.iloc[i, 1:]), label=f't = {data.iloc[i, 0]:.2e}')
     plt.xlabel(r'Rayon du pilier $r$ [m]')
     plt.ylabel(r'Concentration $C$ [mol/m$^3$]')
     plt.title(f'Profil de concentration de sel dans le pilier (N = {len(r)})')
     plt.legend()
     plt.grid(zorder=0)
+    plt.tight_layout()
     plt.show()
-
+    
 plot("output_MMS.csv")
-plot("output.csv")
+plt.savefig("output_MMS.svg")
+plot2("output.csv")
+plt.savefig("output.svg")
 
 
 data_space = pd.read_csv("convergence_results_space.csv")
@@ -86,7 +112,9 @@ def plot_convergence(data, title, space=True):
 delta_t = 4e9/(10000-1)
 title = f"Convergence de l'erreur en fonction de $\Delta r$ pour $\Delta t$ = {delta_t:.2e} s"
 plot_convergence(data_space, title)
+plt.savefig("convergence_space.svg")
 
 delta_r = 0.5/(1000-1)
 title = f"Convergence de l'erreur en fonction de $\Delta t$ pour $\Delta r$ = {delta_r:.2e} m"
 plot_convergence(data_time, title, space=False)
+plt.savefig("convergence_time.svg")
